f67fa268a244141b2361e6b3828131aa8676f13f
Kirill <kirilltanae314@gmail.com>
2024-04-09 20:06:45 +0600

    add tests for yaml and errors

--- __tests__/core.test.js
+++ __tests__/core.test.js
@@ -16,3 +16,24 @@ test('flat json with result in stylish', () => {
 
   expect(generateDifferences(file1Path, file2Path)).toEqual(expected);
 });
+
+test('flat yaml with result in stylish', () => {
+  const file1Path = pathToFixture('flat1.yaml');
+  const file2Path = pathToFixture('flat2.yml');
+  const expected = readFile('stylishFlat.txt');
+
+  expect(generateDifferences(file1Path, file2Path)).toEqual(expected);
+});
+
+test('get errors', () => {
+  const file1Path = pathToFixture('flat1.txt');
+  const file2Path = pathToFixture('flat2.mp3');
+
+  const expected = 'file1 extension must be ".json" or ".yaml" but given ".txt"\n'
+  + 'file2 extension must be ".json" or ".yaml" but given ".mp3"\n'
+  + 'supported formats are "json", "plain" or "stylish" but given "epub"\n'
+  + `file with path "${file1Path}" does not exist\n`
+  + `file with path "${file2Path}" does not exist`;
+
+  expect(generateDifferences(file1Path, file2Path, 'epub')).toEqual(expected);
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,12 +8,12 @@ program
   .description(
     'Compares two configuration files and shows a difference.\n'
   + 'Supported input formats (file extension): "json", "yaml".\n'
-  + 'Supported output formats: "plain text", "stylish" or "json".',
+  + 'Supported output formats: "plain", "stylish" or "json".',
   )
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format. Default is stylish')
-  .argument('<filepath1>', 'path to file1. Can be raltive or absolute')
-  .argument('<filepath2>', 'path to file2. Can be raltive or absolute')
+  .option('-f, --format [type]', 'Output format. Default is stylish')
+  .argument('<filepath1>', 'Path to file1. Can be raltive or absolute')
+  .argument('<filepath2>', 'Path to file2. Can be raltive or absolute')
   .action((filePath1, filePath2) => {
     console.log(generateDifferences(filePath1, filePath2, program.opts().format));
   });

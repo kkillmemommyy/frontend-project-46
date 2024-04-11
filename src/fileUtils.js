8c579f7e096daecc34e0c91a09fa1b83a3f0d2ba
Kirill <kirilltanae314@gmail.com>
2024-04-11 20:58:13 +0600

    fix

--- src/fileUtils.js
+++ src/fileUtils.js
@@ -9,6 +9,7 @@ const isSupportedFormat = (format, stream = 'input') => {
     output: ['json', 'plain', 'stylish'],
   };
   const currentStream = supportedFormats[stream];
+  /* eslint-disable-next-line */
   for (const currentFormat of currentStream) {
     if (format === currentFormat) {
       return true;
